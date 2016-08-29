c99b421c1260391015de09528caa207e93bcb7f6
nikla <nikla@192.168.192.23>
2016-08-29 15:29:51 +0200

    x

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
-	console.log("hello world");
-});
-
 $(window).scroll(function() {
 	if ($(document).scrollTop() > 50) {
 		$('nav').addClass('shrink');
 	} else {
 		$('nav').removeClass('shrink');
 	}
+});
+
+$(document).ready(function() {
+	console.log("hello world");
 });
