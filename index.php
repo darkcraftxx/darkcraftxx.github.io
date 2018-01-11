6c237d756528cbff83ba7722d2adfc04ad1f62fe
Niklas Gerhardt <world.niklas@gmail.com>
2018-01-11 12:27:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 </head>
 
 <body>
-  <header>
+    <header>
       <h1 class="title"><a href="index.php">LastTry</a></h1>
     <div class="nav">
       <ul>
@@ -37,69 +37,8 @@
       </ul>
     </div>
   </header>
-    <div class="content"> 
-        <div class="sidebar-left">
-            <a href="https://www.wowprogress.com/guild/eu/azshara/LastTry"><img alt="WoW Guild Rankings" src="https://wowprogress.com/guild_img/1130626/out/type.site" border="0"/></a>
-        </div>
-        <div class="sidebar-right">
-            <a href="http://eu.battle.net/wow/de/guild/azshara/LastTry/"><img alt="WoW Guild" src="images/lasttry.png" border="0"/></a>
-        </div>
-        <div class="container">
-            <h2 style="margin-top: 40px;"><u>Willkommen bei LastTry - Azshara!</u></h2>
-            <div class="raid">
-                <p>LastTry rekrutiert und wir suchen dich!
-                    Wir sind eine familiäre Gilde die gemütlich aber dennoch erfolgsorientiert raided.
-                    Zum Teil spielen wir schon mehrere Jahre zusammen, und neue Mitglieder werden bei uns herzlich aufgenommen.
-                    Zur Erweiterung unseres Stammraids suchen wir zurzeit:
-                    <ul>
-                        <li> 1 healer
-                        <li> mehrere DDs
-                    </ul>
-                </p>
-                <p>
-                    <div class="left">
-                    <h3> Was wir bieten:</h3>
-                    <ul>
-                        <li>2 feste Raidabende
-                        <li>Freundliche Atmosphäre auch im Progress
-                        <li>Faire Lootvergabe via Lootcouncil
-                        <li>Starke Raidleitung
-                        <li>Ein offenes Ohr für Eure Anliegen
-                    </ul>
-                    </div>
-                    <div class="right">
-                    <h3> Was wir erwarten:</h3>
-                    <ul>
-                        <li>Eine umgängliche erwachsene Einstellung
-                        <li>Klassenverständniss
-                        <li>Keinen Lootneid
-                        <li>Anwesenheit zu den Raids von ca. 80%
-                        <li>Technische Vorausetzungen (Mikrofon, Stabiles Internet, etc.)
-                    </ul>
-                    </div>
-                </p>
-                </br>
-                <div style="margin-top: 180px;">
-                <p>
-                    Unsere Raidabende sind <strong>Montag 21-24 Uhr</strong> (zur Zeit Heroic Clear und danach Mythic progress) und <strong>Mittwoch 21-24 Uhr</strong> (Mythic Farmbosse und danach progress)
-                </p>
-                <div>
-                <hr>
-                <p>
-                    Wenn dich das anspricht und du interesse hast dich uns anzuschließrn kannst du uns <a href="">hier</a> kontaktieren oder im battle.net chat anschreiben.
-                    <ul>
-                        <li><strong>Gildenleitung</strong>: Jackylein#2123
-                        <li><strong>Raidlead</strong>: Soul#2252
-                        <li><strong>Co/Raidlead</strong>: Tugrak#2815
-                        <li><strong>Offi</strong>: Delirious#21796
-                        <li><strong>Offi</strong>: Nemosok#2664
-                    </ul>
-                </p>
-            </div>
-        </div>
-      </div><!-- raid -->
-    </div><!-- container -->
-   </div><!-- content -->
+<?php include("includes/home.html"); ?>
+<?php include("includes/guides.html"); ?>
 
     <!-- jQuery Version 1.11.1 -->
     <script src="js/jquery.min.js"></script>
