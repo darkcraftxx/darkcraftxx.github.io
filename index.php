c171a9b5793e3a7c8199ba9ed1ac349ce5ec9e5c
Niklas Gerhardt <world.niklas@gmail.com>
2018-01-12 11:43:33 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,55 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-    <link rel="icon" href="favicon.ico" type="image/x-icon">
-    
-    <title>LastTry | Azshara</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="css/style.css" rel="stylesheet">
-    
-    <!-- Font Import -->
-    <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet"> 
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet"> 
-    <link href="https://fonts.googleapis.com/css?family=Play" rel="stylesheet"> 
-</head>
-
-<body>
-    <header>
-      <h1 class="title"><a href="index.php">LastTry</a></h1>
-    <div class="nav">
-      <ul>
-        <li class="home"><a<?php if ($pagename == 'index.php') {echo ' class="aktiv"';} ?> href="index.php">Home</a></li>
-        <li class="guides"><a<?php if ($pagename == 'guides.html') {echo ' class="aktiv"';} ?> href="/includes/guides.hmtl">Guides</a></li>
-        <li class="kalender"><a href="#">Kalender</a></li>
-        <li class="kontakt"><a href="#">Kontakt</a></li>
-      </ul>
-    </div>
-  </header>
-
-<?php include("includes/home.html");
-    $_GET["url"]
-    ?>
-
-    <!-- jQuery Version 1.11.1 -->
-    <script src="js/jquery.min.js"></script>
-    
-    <script src="js/script.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-</body>
-
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+      <link rel="icon" href="favicon.ico" type="image/x-icon">
+      <title>LastTry | Azshara</title>
+      <!-- Bootstrap Core CSS -->
+      <link href="css/bootstrap.css" rel="stylesheet">
+      <!-- Custom CSS -->
+      <link href="css/style.css" rel="stylesheet">
+      <!-- Font Import -->
+      <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Play" rel="stylesheet">
+   </head>
+   <body>
+      <header>
+         <h1 class="title"><a href="index.php">LastTry</a></h1>
+         <div class="nav">
+            <ul>
+               <li class="home"><a<?php if ($pagename == 'index.php') {echo ' class="aktiv"';} ?> href="index.php">Home</a></li>
+               <li class="guides"><a<?php if ($pagename == 'guides.html') {echo ' class="aktiv"';} ?> href="/includes/guides.hmtl">Guides</a></li>
+               <li class="kalender"><a href="#">Kalender</a></li>
+               <li class="kontakt"><a href="#">Kontakt</a></li>
+            </ul>
+         </div>
+      </header>
+      <?php include("includes/home.html");
+         $_GET["url"]
+         ?>
+      <!-- jQuery Version 1.11.1 -->
+      <script src="js/jquery.min.js"></script>
+      <script src="js/script.js"></script>
+      <!-- Bootstrap Core JavaScript -->
+      <script src="js/bootstrap.min.js"></script>
+   </body>
 </html>
